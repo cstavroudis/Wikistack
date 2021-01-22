aa1fb38874a50e64eca7b404e7d6cdd3099e4867
Calandra Stavroudis <calandra.st@gmail.com>
2021-01-22 16:15:32 -0500

    adding setting up tables with sequelize

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
+const { db, Page, User } = require('./models');
+
+db.authenticate()
+  .then(() => {
+    console.log('connected to the database');
+  });
 
 //middleware
 app.use(morgan("dev"));
@@ -8,4 +14,21 @@ app.use(express.static(__dirname + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.listen(3000);
+app.get('/', (req, res) => {
+    res.send('Test');
+} )
+
+const PORT = 3000;
+
+const init = async () => {
+    // await db.sync({force: true});
+    await Page.sync();
+    await User.sync();
+    // make sure that you have a PORT constant
+    app.listen(PORT, () => {
+      console.log(`Server is listening on port ${PORT}!`);
+    });
+  }
+  
+  init();
+
